b818ae53c1951ca3ec6f755c27954df50a38983d
Makoto Sugano <makoto.sugano@gmail.com>
2011-08-18 23:52:37 +0300

    Blink added.

--- javascript/main.js
+++ javascript/main.js
@@ -43,10 +43,27 @@ Hazard.prototype.update = function(msDuration) {
     this.rect.moveIp(0, 0);
 };
 
+var Blink = function(rect) {
+    // call superconstructor
+    Blink.superConstructor.apply(this, arguments);
+    this.image = gamejs.image.load("media/blink.png");
+    this.rect = new gamejs.Rect(rect);
+
+    return this;
+};
+
+// inherit (actually: set prototype)
+gamejs.utils.objects.extend(Blink, gamejs.sprite.Sprite);
+Blink.prototype.update = function(msDuration) {
+    // moveIp = move in place
+    this.rect.moveIp(0, 0);
+};
+
 function main() {
 
     var instructionFont = new gamejs.font.Font('20px monospace');
     var trainSound = new gamejs.mixer.Sound('media/probe_shields.ogg');
+    var loop_counter = 0;
 
     function handleEvent(event) {
 	switch(event.type) {
@@ -70,7 +87,6 @@ function main() {
 	};
     };
 
-
     // screen setup
     gamejs.display.setMode([800, 480]);
     gamejs.display.setCaption("jsdashboard");
@@ -78,24 +94,16 @@ function main() {
     // create some background sprites and put them in a group
     // doesn't have to be sprite..
     var background = new Background([0, 0]);
-
     var hazard = new Hazard([400, 240, 48, 48]);
+    var blink = new Blink([400, 160, 48, 48]);
 
     // game loop
     var mainSurface = gamejs.display.getSurface();
-    
-    // msDuration = time since last tick() call
-    var blink = function(msDuration) {
-	
-        // update and draw the backgrounds
-        background.update(msDuration);
-        background.draw(mainSurface);
-    };
-
-    gamejs.time.fpsCallback(tick, this, 20);
 
     // msDuration = time since last tick() call
-    var tick = function(msDuration) {
+    var main_tick = function(msDuration) {
+
+	loop_counter++;
 	
 	gamejs.event.get().forEach(function(event) {
             handleEvent(event);
@@ -105,15 +113,23 @@ function main() {
         // update and draw the backgrounds
         background.update(msDuration);
 	hazard.update(msDuration);
+        blink.update(msDuration);
 
         background.draw(mainSurface);
         hazard.draw(mainSurface);
 
+	if (loop_counter < 20) {
+            blink.draw(mainSurface);
+	} else if (loop_counter == 40) {
+	    loop_counter = 0;
+	};
+
+
+
 	mainSurface.blit(instructionFont.render(
 	    "i love myself", '#0fffff'), [20, 20]);
     };
-
-    gamejs.time.fpsCallback(tick, this, 20);
+    gamejs.time.fpsCallback(main_tick, this, 20);
 }
 
 /**
@@ -121,5 +137,6 @@ function main() {
  */
 gamejs.preload(['media/background.jpg']);
 gamejs.preload(['media/hazard.png']);
+gamejs.preload(['media/blink.png']);
 gamejs.preload(['media/probe_shields.ogg']);
 gamejs.ready(main);
